005464cbc2bd880c3381ad69dea12f29040baff9
Dylan Marciscano <118093596+dylan130102@users.noreply.github.com>
2024-08-13 15:34:27 -0500

    Add files via upload

---
+++ TextEncryptor/script.js
@@ -0,0 +1,35 @@
+function encryptText() {
+    const inputText = document.getElementById("inputText").value;
+
+    const encryptedText = inputText
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+
+    document.getElementById("outputText").value = encryptedText;
+}
+
+function decryptText() {
+    const inputText = document.getElementById("inputText").value;
+
+    const decryptedText = inputText
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ai/g, "a")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+
+    document.getElementById("outputText").value = decryptedText;
+}
+
+function copyToClipboard() {
+    const outputText = document.getElementById("outputText");
+    outputText.select();
+    outputText.setSelectionRange(0, 99999); // Para móviles
+
+    document.execCommand("copy");
+
+    alert("Texto copiado: " + outputText.value);
+}
